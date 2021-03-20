bba4dae74c10b18c207a241b61a1a819665a91ba
Vlad-Vlaicu <vladvlaicu99@gmail.com>
2021-03-20 19:40:16 +0200

    init

---
+++ NYSA-master/app/src/main/java/nysa/nysa_20/activity/MainActivity.java
@@ -0,0 +1,75 @@
+package nysa.nysa_20.activity;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import nysa.nysa_20.R;
+import nysa.nysa_20.model.Toolbar_MainActivity;
+import nysa.nysa_20.model.adaptors.MainActivityPagerAdaptor;
+
+public class MainActivity extends AppCompatActivity {
+    private static ViewPager trackPager;
+    private static MainActivityPagerAdaptor trackAdapter;
+    private static Toolbar_MainActivity toolbar_mainActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initComponents();
+
+        //toolbar stays bottom
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+        //LocationService service = new LocationService(MainActivity.this);
+
+
+
+
+
+    }
+
+    private   void initComponents(){
+        trackAdapter = new MainActivityPagerAdaptor(getSupportFragmentManager());
+        trackPager = findViewById(R.id.trackPager);
+        toolbar_mainActivity = findViewById(R.id.toolbarMainActivity);
+
+        trackPager.setAdapter(trackAdapter);
+
+        prepareViewPager();
+
+
+    }
+
+    private void prepareViewPager() {
+        trackPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                toolbar_mainActivity.updateToolbarImageResources(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+    public static void setCurrentTab(int position){
+        trackPager.setCurrentItem(position);
+        toolbar_mainActivity.updateToolbarImageResources(position);
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Nysa
+# NYSA by Vlaicu Vlad and Matei Andrei
+
+Nysa is a mobile app for Android meant to act as an interface between the medic and patient regarding alergies. The user 
+can record different symptoms and the info is stored internally and can be shared ( through a QR code) with the medic.
